e211e1eef00475774fe02aa862b8ee1f5baf85a7
Zulkifli Fauzi <zulkifli.fauzi@mitrais.com>
2012-05-09 07:43:39 +0800

    Add picture

---
+++ WebUI/Models/ViewModelPicture.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebUI.Models
+{
+    public class ViewModelPicture
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+
+        public int Product_ProductId { get; set; }
+    }
+}
